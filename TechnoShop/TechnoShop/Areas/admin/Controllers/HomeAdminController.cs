ba6db6bbcf6adb55e355758c7051b85e319e1807
DESKTOP-1DHNMIK\Le Thang Henry <gundam00driver@gmail.com>
2023-03-25 21:32:36 +0700

    Tao Home Controller

---
+++ TechnoShop/TechnoShop/Areas/admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechnoShop.Areas.admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: admin/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
