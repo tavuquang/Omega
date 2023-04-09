dd50cb263ac38f787d9c0755e8db700e47f92431
DESKTOP-1DHNMIK\Le Thang Henry <gundam00driver@gmail.com>
2023-04-09 09:40:43 +0700

    Home controller

--- TechnoShop/TechnoShop/Controllers/HomeController.cs
+++ TechnoShop/TechnoShop/Controllers/HomeController.cs
@@ -1,16 +1,41 @@
-﻿using System;
+﻿using TechnoShop.Models;
+using TechnoShop.DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TechnoShop.Models.ViewModel;
+using PagedList;
 
 namespace TechnoShop.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
+        public ActionResult Index(ProductListViewModel model, int? page, string searchString)
+        {   
+            List<Product> listProduct = new List<Product>();
+            if (!String.IsNullOrWhiteSpace(searchString))
+            {
+                listProduct = ProductDAO.SearchProductByName(searchString.Trim());
+            }
+            else
+            {
+                listProduct = ProductDAO.GetList();
+            }
+            List<Category> listCategory = CategoryDAO.GetList();
+            ViewBag.ListCategory = listCategory;
+            int pageNumber = (page ?? 1);
+            int pageSize = 12;
+            ViewBag.ListProduct = listProduct.Count;
+            var r = new Random();
+            ViewBag.ListRandomProduct = listProduct.OrderBy(u => r.Next()).Take(9);
+            model.PageList = listProduct.ToList().ToPagedList(pageNumber, pageSize);
+
+            ViewBag.Dropdown = new SelectList(CategoryDAO.GetList(), "Category_ID", "Category_Name");
+            ViewBag.Max = ProductDAO.GetMax();
+            ViewBag.Min = ProductDAO.GetMin();
+            return View(model);
         }
 
         public ActionResult About()
